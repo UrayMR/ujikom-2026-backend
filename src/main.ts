d3772cd68e1219350bd557375d78500ca5d338f5
UrayMR <umrajsyafiq@gmail.com>
2026-09-05 21:39:10 +0700

    feat: implement ApiResponseDto and TransformInterceptor for consistent API response structure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+
+  // Api Response Transform Interceptor
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(process.env.APP_PORT ?? 3000);
 }
 await bootstrap();
